90344b5df56f183219f89c16b36fad4cf6a92e10
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-07-07 17:11:17 +0300

    package: adds commonjs support and prettier before build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,20 @@
+// rollup.config.js
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "dist/index.js",
-    format: "esm",
-  },
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm", // ES module
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.cjs",
+      format: "cjs", // CommonJS
+      sourcemap: true,
+    },
+  ],
   plugins: [resolve(), commonjs()],
 };
